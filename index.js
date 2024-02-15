248bceb828a76f38a219cffc15a8c3b7859545a1
Kelechikizito <kaylaychi77@gmail.com>
2024-02-15 06:46:22 +0100

    js update

--- index.js
+++ index.js
@@ -7,14 +7,19 @@ let form = $('form');
 let inputField = $('input');
 let button = $('button');
 
+let map;
 
 function mapper(lat, long) {
-    let map = L.map('map').setView([lat, long], 13);
-    
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+    if (!map) {
+        map = L.map('map').setView([lat, long], 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+    } else {
+        map.setView([lat, long], 13);
+    }
 
     const locationIcon = L.icon({
         iconUrl: 'images/icon-location.svg',
@@ -22,30 +27,29 @@ function mapper(lat, long) {
         iconAnchor: [15, 15]
     });  
 
-    L.marker([lat, long], {icon: locationIcon}).addTo(map)
+    L.marker([lat, long], {icon: locationIcon}).addTo(map);
 }
 
 function getUserIP(api) {
     fetch(api)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        console.log(data);
         mapper(data.latitude, data.longitude);
         ipAddress.text(data.ip);
         userLocation.text(`${data.city}, ${data.country_name}`);
-        userTimezone.text(`UTC ${data.utc_offset.slice(0,3)}:00`)
+        userTimezone.text(`UTC ${data.utc_offset.slice(0,3)}:00`);
         userISP.text(data.org);
     })
     .catch(err => {
-        console.error('Error fetching IP address', err)
-    })
+        alert(err);
+        console.error(err);
+    });
 }
+
 getUserIP(`https://ipapi.co/json`);
 
-
 button.click(function (e) { 
-    
-    getUserIP(`https://ipapi.co/${inputField.val()}/json/`);
+    getUserIP(`https://ipapi.co/${inputField.val()}/json`);
     e.preventDefault();
-    
 });
